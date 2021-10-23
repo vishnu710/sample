eabacc343221a74a64c4cbaafecd7fdacb1d8f7a
vishnu710 <vishnu72001@gmail.com>
2021-10-23 16:19:14 +0530

    first commit

---
+++ api/users/user.router.js
@@ -0,0 +1,17 @@
+const { 
+    createUser,
+    getUserByUserId,
+    getUsers,
+    updateUser,
+    deleteUser,
+    login
+} = require("./user.controller");
+const router = require("express").Router();
+
+router.post("/",createUser);
+router.get("/",getUsers);
+router.get("/:id",getUserByUserId );
+router.patch("/",updateUser);
+router.delete("/", deleteUser);
+router.post("/login",login)
+module.exports = router;
